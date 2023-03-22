90408de0bf8f2e282a8eecff2c17d6e12d963613
Astraeus-Q <1521050540@qq.com>
2023-03-22 20:43:54 +0800

    Add Plots

--- Clicker_recv.py
+++ Clicker_recv.py
@@ -1,12 +1,14 @@
 '''
     Senior Project: Clicker for ZJUI Undergraduate
     Date: 3-11-2023
-    Version: 1.0
+    Version: 1.1
 '''
 
 import serial
 import time
-
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 
 ID_LEN = 4 # ID of Clicker is a 4-digit number. Like"1234".
 ANS_LEN = 1 # 'A', 'B', 'C', 'D', 'E', 'F'
@@ -69,16 +71,51 @@ def USB_read(port:str, timelim)->Clicker_resp:
     print("Section End (^v^)/")
 
 
+def plot_answer(stu_ans:dict, correct):
+    plt.title("Student Answers")
+    plt.xlabel("Options")
+    plt.ylabel("Number")
+
+    options = ['A', 'B', 'C', 'D', 'E']
+    ans = list(stu_ans.values())
+    num = [ans.count(x) for x in options]
+
+    r_g = ["green" if op == correct else "red" for op in options]
+
+    print(r_g)
+
+    plt.bar(options, num, color = r_g)
+
+    for x,y in zip(options, num):   
+        plt.text(x, y, '%d'%y, ha='center', va='bottom', fontsize=7)
+    plt.show()
+    return
+
+def plot_attendance():
+    y = [72, 28]
+    plt.pie(y, labels = ["Present", "Absent"], autopct='%d')
+    plt.title("Attendance: %.2f%%" % (100*y[0]/sum(y)))
+    plt.show()
+    return
 
 def main():
     timelim = 15
-    database = {}
+    stu_ans = {}
+
+    '''
     port = "COM6"
     for c in USB_read(port, timelim):
         print("ID: %d, Ans: %c" % (c.id, c.ans))
-        database[c.id] = c.ans
+        stu_ans[c.id] = c.ans
+    '''
+
+    # Random Ans Test
+    for i in range(100):
+        stu_ans[i] = random.choice(['A', 'B', 'B', 'C', 'D', 'E'])
     print("Response:")
-    print(database)
+    correct = 'B'
+    plot_answer(stu_ans, correct)
+    plot_attendance()
     return
 
 if __name__ == '__main__':
