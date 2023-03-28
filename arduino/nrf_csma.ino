390d4354993fcf59470696a34c8246d64f3d531d
qishenz2 <qishenz2.illinios.edu>
2023-03-28 17:01:14 +0800

    CSMA test file added.

--- arduino/nrf_csma.ino
+++ arduino/nrf_csma.ino
@@ -4,11 +4,13 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <cstdlib>
+#include <string>
 // not used in final compile.
 #include <cstdint>
 // add arduino SPI.
 #define DELAY 1000 // 1 second.
 #define DIFS 50     // 50 ms
+#define CCA 1      // 1ms for CCA window.
 // constructor RF24
 RF24 radio(7,8); // enable Chip-enable and Chip select.
 
@@ -17,16 +19,9 @@ uint8_t address[][6] = {"1Node","2Node"};
 
 uint8_t radioAddr = 1; // Using # address to Transmit
 
-uint8_t NodeID = 0;
-
-uint8_t count_down = 0;
-
-uint8_t col_count = 0;
-
-uint8_t window[7] = {1,7,15,31,63,127,255};
+int8_t NodeID = -1;
 
 bool Rxmode = 0; // True for rx mode, false for Tx mode.
-
 
 typedef struct Payload_clicker{
     char msg[7];
@@ -46,23 +41,95 @@ void setPayload(clickerload& pay, char* message) {
     pay.counter = 0;
 }
 
-void backoff(bool success){
 
-    if (success){
-        col_count = 0;
-    } else{
+// we can modify the void* to a list then we can transmit multiple payload.
+
+/*
+ *  unslotted_trans
+ *  transmit the given payload packet using csma/ca By IEEE.
+ *
+ *  @param payload the given payload packet.
+ *  @param NodeID given node ID to transmit.
+ *
+ *  @return
+ *  - 'counts' collision counts if success,
+ *  - '-1' if failure.
+ */
+int8_t unslotted_trans(void* payload ,int8_t NodeID = -1){
+
+    uint8_t count_down = 0;
+    int8_t col_count = 0;
+    const uint8_t max_col = 10;
+    const uint8_t win_size = 7;
+    uint8_t window[7] = {1,7,15,31,63,127,255};
+
+    bool approve = false;
+    bool success = false;
+
+    radio.flush_tx();
+    radio.startListening();
+
+    // First Transmission Trial.
+    if (!radio.testRPD()){
+        delayMicroseconds(CCA);
+        if (!radio.testRPD()){
+            approve = true;
+        } else {
+            // collision in CCA window. Maybe useful in further enhancement.
+        }
+    }
+    // First trial failed. Add a backoff window.
+    if (!approve){
+        if (NodeID != -1){
+            srand(NodeID);
+        }else{
+            srand(rand());
+        }
         col_count ++;
+        count_down = rand()%(window[col_count]);
     }
-    // calculate the current backoff count.
-    if (!NodeID){
-        // NodeID not been set!
-        srand(radio.getChannel());
+
+    // Loop Trial until success or max count
+    while(!success || col_count >= max_col){
+        if (!approve){
+            delayMicroseconds(DIFS);
+            if (!radio.testRPD()){
+                delayMicroseconds(CCA);
+                if (!radio.testRPD()){
+                    count_down--;
+                }
+            }
+        }
+        // count down drop to zero.
+        if (!count_down){
+            radio.stopListening();
+            bool report = radio.write(&payload, sizeof(clickerload));
+            if (report){
+                success = true;
+            }else{
+                // Timeout happened. collision.
+                col_count ++;
+                if (col_count < win_size){
+                    count_down = rand()%window[col_count];
+                } else{
+                    count_down = rand()%window[win_size];
+                }
+                success = false;
+                approve = false;
+                radio.reUseTX();
+                radio.startListening();
+            }
+        }
     }
-    if (col_count>6){
-        col_count = 6;
+    // finalize transmission.
+    radio.txStandBy();
+    if (success) {
+        return col_count;
+    } else{
+        return -1;
     }
-    count_down = rand()%(window[col_count]);
 }
+
 
 void setup(){
 
@@ -74,23 +141,21 @@ void setup(){
 
     // call radio begin to setup chip.
     if (!radio.begin()){
-        Serial.println(F("code example!"))
+        Serial.println(F("Testing Code for CSMA function "))
         while(1){};
         // need to restart the chip.
     }
 
     // State have already set to Tx mode.
-    Serial.println(F("Chip Start With CSMA.ino"));
+    Serial.println(F("Chip Start With CSMA testing mode. "));
 
     // may set to automatic issued ID in the future
     Serial.println(F("Enter The node id. Default is 0. "));
-    while (!Serial.available()){};
+    while (!Serial.available()){}
 
-    // TODO: check the IClicker bottom setting.
-
-    char input = Serial.parseint();
+    char input = Serial.parseInt();
     NodeID = (int)input;
-    srand(NodeID);
+
     Serial.print(F("NodeID = "));
     Serial.println(NodeID);
 
@@ -98,10 +163,8 @@ void setup(){
     // set payload
     radio.setPayloadSize(sizeof(clickerload));
     // remember test b4 set to max !!
-    radio.setPALevel(RF24_PA_MIN);
-
+    radio.setPALevel(RF24_PA_LOW);
     // set read and write pipeline. radio Addr = 1
-    // avoid using pipe 0 to RP.
     radio.openReadingPipe(1,address[radioAddr]);
     radio.openWritingPipe(address[!radioAddr]);
 
@@ -113,35 +176,41 @@ void setup(){
 // functions here.
 
 void loop(){
-    // check the carrier status.
-    if (count_down == 0){
-        count_down = 1;
-        // increase the countdown to 1.
+    int test = unslotted_trans(&Test_load,NodeID);
+    if (test != -1){
+        Serial.print(F("Trans Success! Collision times = "));
+        Serial.println((int)test);
+        Test_load.counter++;
+    } else{
+        Serial.println(F("Max retried times reached!"));
     }
-
-    if (!radio.testRPD()){
-        // channel empty. Transmit to Node 0.
-        count_down --;
-        if (!count_down){
-            radio.stopListening();
-            delay(DIFS);
-            bool report = radio.write(Test_load, sizeof(clickerload));
-            if (report){
-                Serial.print(F("Sent. count: "));
-                Serial.println(Test_load.counter);
-                Test_load.counter++;
-                // reset the counter.
-                backoff(1);
-            } else {
-                Serial.println(F("Trans Timeout!"));
-                // reset the counter.
-                backoff(0);
-            }
-            radio.startListening();
-        }
-    }
-
     delay(DELAY);
 }
 
+// Handshake with the server and read about the node ID and current class.
+uint8_t pair(uint8_t* base_addr){
+    // assume an ID was attached to the arduino flash.
+    radio.toggleAllPipes(false);
+    radio.openWritingPipe(base_addr); // always use pipe 0;
+    radio.openReadingPipe(1,base_addr);
 
+    clickerload handshake;
+    char message = NodeID+'0';
+    setPayload(handshake,&message);
+
+    // transmit the payload to the base address.
+    int8_t report = unslotted_trans(&handshake,NodeID);
+    if (report >= 0){
+        // handshake successful.
+        radio.startListening();
+        Rxmode = 1;
+        // drop into rx mode.
+    }else{
+        return -1;
+    }
+
+    // receive for the handshake message.
+
+    // TODO: Under investigation. We need to design the pairing model.
+
+}
